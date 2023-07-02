aaf006014ff51dd01cf67dacd6feaa4d2433b6a3
Pedro Cobucci <PedroCobucci@gmail.com>
2023-07-02 17:58:57 -0300

    google button test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  function handleCredentialResponse(response: any){
+    console.log(response)
+  }
+
+
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: 'YOUR_GOOGLE_CLIENT_ID',
+      callback: handleCredentialResponse
+    });
+
+    const parent = document.getElementById('google_btn');
+    google.accounts.id.renderButton(parent, {theme: "filled_blue"});
+  }, [])
 
   return (
     <>
@@ -17,6 +35,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div id='google_btn'>
+
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +49,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <script src="https://accounts.google.com/gsi/client" async></script>
     </>
   )
 }
