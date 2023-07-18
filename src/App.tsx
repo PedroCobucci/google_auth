72a92d65476d23b6273c4f79169c9789960ac11a
Jonas <jonasreis74@hotmail.com>
2023-07-18 17:54:09 -0300

    Pequenos ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -28,14 +26,14 @@ function App() {
     });
 
     const parent = document.getElementById('google_btn');
-    google.accounts.id.renderButton(parent, { theme: 'filled_blue' });
+    google.accounts.id.renderButton(parent, { theme: 'filled_blue', size: 'large'});
   }, []);
 
   return (
     <>
-      <h1> Trabalho Sistemas Distribuidos </h1>
-      <h2> LOGIN </h2>
-      
+      <h1>Trabalho Sistemas Distribuidos</h1>
+      <h2>LOGIN</h2>
+
       <div id="fonte">
         <label htmlFor="userLogin">Usuario</label>
         <br></br>
@@ -49,16 +47,16 @@ function App() {
 			</div>
 
       <div>
-        <button className="logar" type="submit" value="Entrar"> Entrar </button>
+        <button className="logar" type="submit" value="Entrar">Entrar</button>
       </div>
 
-      {isLoggedIn ? (
+      {isLoggedIn && 
+        <div>
+          <h2>Você está logado!</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      }
 
-          <div>
-            <h2>Você está logado!</h2>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-      ) : (<></>)}
       <div style={{display: 'flex', paddingTop: '5%', justifyContent: 'center'}} id="google_btn"></div>
       <div className="card">
         <p>
